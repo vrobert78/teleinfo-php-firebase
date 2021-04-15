29a77bd021ee722cd208e1824ad8f127be6ef39f
Vincent Robert <vincent9.robert@gmail.com>
2021-04-15 15:24:18 +0200

    add production to statsdé

--- teleinfo-production.php
+++ teleinfo-production.php
@@ -36,10 +36,16 @@ $database = (new Factory)
 $memcacheD = new Memcached;
 $memcacheD->addServer(MEMCACHED_SERVER, MEMCACHED_PORT);
 
+$connection = new \Domnikl\Statsd\Connection\UdpSocket(STATSD_SERVER, STATSD_PORT);
+$statsd = new \Domnikl\Statsd\Client($connection, 'HOMETIC-PRODUCTION');
+
 while (true) {
     try {
         $arrayValues = getTeleinfo();
         if (DEBUG) var_dump($arrayValues);
+
+        $statsd->gauge('SINSTI', $arrayValues['SINSTI']);
+
         $return = insertIntoFirebase($arrayValues,$database);
         if (DEBUG) echo 'Insert:'.$return.PHP_EOL;
 
