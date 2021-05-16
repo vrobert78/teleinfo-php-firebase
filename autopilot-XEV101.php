d0ab22594cf54d0ab10c6f0564d2017166fcf2d4
Vincent Robert <vincent9.robert@gmail.com>
2021-05-16 17:03:11 +0200

    tunning

--- autopilot-XEV101.php
+++ autopilot-XEV101.php
@@ -54,6 +54,7 @@ $count=0;
 $ISOUSC=MIN_POWER_XEV;
 $oldState='UNKNOWN';
 $newState='UNKNOWN';
+$max_loop_before_decrease=MAX_LOOPS_BEFORE_DECISION_DECREASE;
 
 while (true) {
     if (DEBUGAUTOPILOT) echo "----------".PHP_EOL;
@@ -71,110 +72,116 @@ while (true) {
 
     $enphaseArray = $memcacheD->get('ENPHASE');
 
-    $PRODUCTION = intval($enphaseArray['production'][0]['wNow']);
-    $PRODUCTIONA = intval($PRODUCTION / VOLTAGE);
+    if (array_key_exists('production', $enphaseArray)) {
 
-    $statsd->gauge('PRODUCTION', $PRODUCTION);
-    $statsd->gauge('PRODUCTIONA', $PRODUCTIONA);
+        $PRODUCTION = intval($enphaseArray['production'][0]['wNow']);
+        $PRODUCTIONA = intval($PRODUCTION / VOLTAGE);
 
-    if (DEBUGAUTOPILOT) echo "Production: $PRODUCTIONA A".PHP_EOL;
+        $statsd->gauge('PRODUCTION', $PRODUCTION);
+        $statsd->gauge('PRODUCTIONA', $PRODUCTIONA);
 
-    if ($PAPP===0) {
-        if (DEBUGAUTOPILOT) echo "Injection: $IINST A".PHP_EOL;
-        $statsd->gauge('EXPORTA', $IINST);
-        $statsd->gauge('EXPORT', $IINST*VOLTAGE);
-        $statsd->gauge('IMPORTA', 0);
-        $statsd->gauge('CONSOMMATION', $PRODUCTION-($IINST*VOLTAGE));
-        $statsd->gauge('CONSOMMATIONA', $PRODUCTIONA-$IINST);
-    }
-    else {
-        if (DEBUGAUTOPILOT) echo "Import: $IINST A - $PAPP VA".PHP_EOL;
-        $statsd->gauge('EXPORTA', 0);
-        $statsd->gauge('EXPORT', 0);
-        $statsd->gauge('IMPORTA', $IINST);
-        $statsd->gauge('CONSOMMATION', $PAPP+$PRODUCTION);
-        $statsd->gauge('CONSOMMATIONA', $PRODUCTIONA+$IINST);
-    }
-
-    $TRAME_PAPP = "00000$PAPP";
-    $TRAME_PAPP = substr($TRAME_PAPP, strlen($TRAME_PAPP)-5);
-
-    $STOP = false;
-
-    if ($PRODUCTIONA>=MIN_POWER_XEV || ($PAPP===0 && $IINST>=MIN_POWER_XEV)) {
+        if (DEBUGAUTOPILOT) echo "Production: $PRODUCTIONA A".PHP_EOL;
 
         if ($PAPP===0) {
-            if (DEBUGAUTOPILOT) echo "Production avec Injection".PHP_EOL;
-            $newState = 'INJECTION';
-            if ($oldState!=$newState) $count = 0;
+            if (DEBUGAUTOPILOT) echo "Injection: $IINST A".PHP_EOL;
+            $statsd->gauge('EXPORTA', $IINST);
+            $statsd->gauge('EXPORT', $IINST*VOLTAGE);
+            $statsd->gauge('IMPORTA', 0);
+            $statsd->gauge('CONSOMMATION', $PRODUCTION-($IINST*VOLTAGE));
+            $statsd->gauge('CONSOMMATIONA', $PRODUCTIONA-$IINST);
+        }
+        else {
+            if (DEBUGAUTOPILOT) echo "Import: $IINST A - $PAPP VA".PHP_EOL;
+            $statsd->gauge('EXPORTA', 0);
+            $statsd->gauge('EXPORT', 0);
+            $statsd->gauge('IMPORTA', $IINST);
+            $statsd->gauge('CONSOMMATION', $PAPP+$PRODUCTION);
+            $statsd->gauge('CONSOMMATIONA', $PRODUCTIONA+$IINST);
+        }
+
+        $TRAME_PAPP = "00000$PAPP";
+        $TRAME_PAPP = substr($TRAME_PAPP, strlen($TRAME_PAPP)-5);
+
+        $STOP = false;
+
+        if ($PRODUCTIONA>=MIN_POWER_XEV || ($PAPP===0 && $IINST>=MIN_POWER_XEV)) {
+
+            if ($PAPP===0) {
+                if (DEBUGAUTOPILOT) echo "Production avec Injection".PHP_EOL;
+                $newState = 'INJECTION';
+                if ($oldState!=$newState) $count = 0;
 
 
-            if ($count>=MAX_LOOPS_BEFORE_DECISION) {
-                $count = 0;
+                if ($count>=MAX_LOOPS_BEFORE_DECISION) {
+                    $count = 0;
 
-                if ($ISOUSC<min(MAX_ISOUSC,$PRODUCTIONA*2) && $IINST>MIN_INJECTION) {
-                    $ISOUSC++;
+                    if ($ISOUSC<MAX_ISOUSC && $IINST>MIN_INJECTION) {
+                        $ISOUSC++;
+                    }
+                    elseif ($IINST<MIN_INJECTION && $ISOUSC>1) {
+                        $ISOUSC--;
+                    }
                 }
-                elseif ($IINST<MIN_INJECTION && $ISOUSC>1) {
-                    $ISOUSC--;
+            }
+            else {
+                if (DEBUGAUTOPILOT) echo "Production + Import".PHP_EOL;
+                $newState = 'IMPORT';
+                if ($oldState!=$newState) {
+                    $count = 0;
+                    $max_loop_before_decrease = MAX_LOOPS_BEFORE_DECISION_DECREASE;
                 }
-                elseif ($ISOUSC>min(MAX_ISOUSC,$PRODUCTIONA*2)) {
-                    $ISOUSC--;
+                else {
+                    if ($max_loop_before_decrease>=1) $max_loop_before_decrease--;
                 }
+
+                if ($count>=$max_loop_before_decrease) {
+                    $count = 0;
+
+                    if ($ISOUSC>1) {
+                        $ISOUSC--;
+                    }
+                }
+
             }
         }
         else {
-            if (DEBUGAUTOPILOT) echo "Production + Import".PHP_EOL;
-            $newState = 'IMPORT';
-            if ($oldState!=$newState) $count = 0;
+            if (DEBUGAUTOPILOT) echo "Peu ou Pas de Production > STOP".PHP_EOL;
+            $newState = 'NO-PRODUCTION';
+            $count = 0;
 
-            if ($count>=MAX_LOOPS_BEFORE_DECISION) {
-                $count = 0;
-
-                if ($ISOUSC>1) {
-                    $ISOUSC--;
-                }
-            }
-
+            $STOP=true;
         }
+
+        if (DEBUGAUTOPILOT) echo "ISOUSC: $ISOUSC".PHP_EOL;
+        if (DEBUGAUTOPILOT) echo "STATE: $newState".PHP_EOL;
+
+        if ($STOP || $ISOUSC<=MIN_POWER_XEV) {
+            $TRAME_ISOUSC = "01";
+            $TRAME_IINST = "003";
+            $TRAME_ADPS = "003";
+            $TRAME_PTEC = "HP..";
+
+            $ISOUSC=MIN_POWER_XEV;
+        }
+        else {
+            $TRAME_ISOUSC = "00$ISOUSC";
+            $TRAME_ISOUSC = substr($TRAME_ISOUSC, strlen($TRAME_ISOUSC)-2);
+
+            $TRAME_IINST = "000";
+            $TRAME_ADPS = "000";
+            $TRAME_PTEC = "HC..";
+        }
+
+        $statsd->gauge('ISOUSC', intval($TRAME_ISOUSC));
+
+        if (DEBUGAUTOPILOT) echo "TRAME_PAPP: $TRAME_PAPP".PHP_EOL;
+        if (DEBUGAUTOPILOT) echo "TRAME_ISOUSC: $TRAME_ISOUSC".PHP_EOL;
+        if (DEBUGAUTOPILOT) echo "TRAME_IINST: $TRAME_IINST".PHP_EOL;
+        if (DEBUGAUTOPILOT) echo "TRAME_ADPS: $TRAME_ADPS".PHP_EOL;
+        if (DEBUGAUTOPILOT) echo "TRAME_PTEC: $TRAME_PTEC".PHP_EOL;
+
+        sendFrame($TRAME_PAPP, $TRAME_PTEC, $TRAME_IINST, $TRAME_ISOUSC, $TRAME_ADPS);
     }
-    else {
-        if (DEBUGAUTOPILOT) echo "Peu ou Pas de Production > STOP".PHP_EOL;
-        $newState = 'NO-PRODUCTION';
-        $count = 0;
-
-        $STOP=true;
-    }
-
-    if (DEBUGAUTOPILOT) echo "ISOUSC: $ISOUSC".PHP_EOL;
-    if (DEBUGAUTOPILOT) echo "STATE: $newState".PHP_EOL;
-
-    if ($STOP || $ISOUSC<=MIN_POWER_XEV) {
-        $TRAME_ISOUSC = "01";
-        $TRAME_IINST = "003";
-        $TRAME_ADPS = "003";
-        $TRAME_PTEC = "HP..";
-
-        $ISOUSC=MIN_POWER_XEV;
-    }
-    else {
-        $TRAME_ISOUSC = "00$ISOUSC";
-        $TRAME_ISOUSC = substr($TRAME_ISOUSC, strlen($TRAME_ISOUSC)-2);
-
-        $TRAME_IINST = "000";
-        $TRAME_ADPS = "000";
-        $TRAME_PTEC = "HC..";
-    }
-
-    $statsd->gauge('ISOUSC', intval($TRAME_ISOUSC));
-
-    if (DEBUGAUTOPILOT) echo "TRAME_PAPP: $TRAME_PAPP".PHP_EOL;
-    if (DEBUGAUTOPILOT) echo "TRAME_ISOUSC: $TRAME_ISOUSC".PHP_EOL;
-    if (DEBUGAUTOPILOT) echo "TRAME_IINST: $TRAME_IINST".PHP_EOL;
-    if (DEBUGAUTOPILOT) echo "TRAME_ADPS: $TRAME_ADPS".PHP_EOL;
-    if (DEBUGAUTOPILOT) echo "TRAME_PTEC: $TRAME_PTEC".PHP_EOL;
-
-    sendFrame($TRAME_PAPP, $TRAME_PTEC, $TRAME_IINST, $TRAME_ISOUSC, $TRAME_ADPS);
 
     sleep(1);
 }
